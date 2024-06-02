dfc240e21d9d653041c54feb48cd176eb6ae8333
MatundingCalculator <zianwayne14@gmail.com>
2024-06-02 23:23:08 +0800

    Initial commit

---
+++ app/src/main/java/com/example/abendaniomulticalc/Buttons.java
@@ -0,0 +1,14 @@
+package com.example.abendaniomulticalc;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Buttons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_buttons);
+    }
+}
